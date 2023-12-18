49f5a852d30332f2de1afd3dc517bb73b6a5725a
Benas M <benasmiliunas@gmail.com>
2023-12-18 19:40:11 +0200

    Configure left rail

--- ui/main.py
+++ ui/main.py
@@ -23,26 +23,19 @@ class ScrollableLabelButtonFrame(ct.CTkScrollableFrame):
         self.grid_columnconfigure(0, weight=1)
 
         self.command = command
-        self.radiobutton_variable = ct.StringVar()
-        self.label_list = []
         self.button_list = []
 
-    def add_item(self, item, image=None):
-        label = ct.CTkLabel(self, text=item, image=image, compound="left", padx=5, anchor="w")
-        button = ct.CTkButton(self, text="Command", width=100, height=24)
+    def add_item(self, item):
+        button = ct.CTkButton(self, text=item, height=24, width=240)
         if self.command is not None:
             button.configure(command=lambda: self.command(item))
-        label.grid(row=len(self.label_list), column=0, pady=(0, 10), sticky="w")
-        button.grid(row=len(self.button_list), column=1, pady=(0, 10), padx=5)
-        self.label_list.append(label)
+        button.grid(row=len(self.button_list), column=0, pady=(0, 10), padx=5,)
         self.button_list.append(button)
 
     def remove_item(self, item):
-        for label, button in zip(self.label_list, self.button_list):
+        for label, button in self.button_list:
             if item == label.cget("text"):
-                label.destroy()
                 button.destroy()
-                self.label_list.remove(label)
                 self.button_list.remove(button)
                 return
 
@@ -69,12 +62,11 @@ class App(ct.CTk):
                                                                         command=self.label_button_frame_event,
                                                                          fg_color="transparent",
                                                                         corner_radius=0)
-        self.scrollable_label_button_frame1.grid(row=1, column=1, padx=0, pady=0, sticky="nsew")
+        self.scrollable_label_button_frame1.grid(row=1, column=1, padx=0, pady=20, sticky="nsew")
         for i in range(10):  # add items with images
-            self.scrollable_label_button_frame1.add_item(f"image and item {i}", image=ct.CTkImage(
-                Image.open(os.path.join(current_dir, "chat_light.png"))))
+            self.scrollable_label_button_frame1.add_item(f"image and item {i}")
 
-        upload_button = ct.CTkButton(left_rail, text="Upload", fg_color="#008000", hover_color='#004d00')
+        upload_button = ct.CTkButton(left_rail, height=40, text="Upload diagram", fg_color="#008000", hover_color='#004d00')
         upload_button.grid(row=2, column=1, padx=30, pady=30, sticky="nsew")
 
         right_rail = ct.CTkFrame(master=self, width=300, corner_radius=4)
