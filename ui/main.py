37a17c01adfb4a06b656afb34e6a4a70388961bc
Benas M <benasmiliunas@gmail.com>
2023-12-21 10:13:16 +0200

    Configure frames

--- ui/main.py
+++ ui/main.py
@@ -3,20 +3,6 @@ import customtkinter as ct
 import os
 from PIL import Image
 
-'''
-ct.set_appearance_mode("dark")
-
-root = ct.CTk()
-root.geometry('1024x640')
-
-test = ct.CTkFrame()
-
-upload_btn = ct.CTkButton(root, text="Upload diagram").place(relx=0.5, rely=0.5, anchor=CENTER)
-
-root.mainloop()
-'''
-
-
 class ScrollableLabelButtonFrame(ct.CTkScrollableFrame):
     def __init__(self, master, command=None, **kwargs):
         super().__init__(master, **kwargs)
@@ -47,6 +33,7 @@ class App(ct.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.columnconfigure(2, weight=1)
 
+        # Left rail
         left_rail = ct.CTkFrame(self, width=300, corner_radius=4)
         left_rail.columnconfigure(0)
         left_rail.rowconfigure(0)
@@ -57,7 +44,6 @@ class App(ct.CTk):
         app_label.grid(row=0, column=1, padx=15, pady=15)
 
         # create scrollable label and button frame
-        current_dir = os.path.dirname(os.path.abspath(__file__))
         self.scrollable_label_button_frame1 = ScrollableLabelButtonFrame(master=left_rail, width=300,
                                                                         command=self.label_button_frame_event,
                                                                          fg_color="transparent",
@@ -69,8 +55,17 @@ class App(ct.CTk):
         upload_button = ct.CTkButton(left_rail, height=40, text="Upload diagram", fg_color="#008000", hover_color='#004d00')
         upload_button.grid(row=2, column=1, padx=30, pady=30, sticky="nsew")
 
-        right_rail = ct.CTkFrame(master=self, width=300, corner_radius=4)
-        right_rail.grid(row=0, column=2, padx=15, pady=15, sticky="nsew")
+        # Right rail
+        right_rail = ct.CTkFrame(master=self, corner_radius=4, fg_color="transparent")
+        right_rail.grid(row=0, column=2, padx=(7.5, 15), pady=15, sticky="nsew")
+        right_rail.grid_rowconfigure(1, weight=1)
+        right_rail.grid_columnconfigure(0, weight=1)
+
+        right_rail_top = ct.CTkFrame(master=right_rail, height=40)
+        right_rail_top.grid(row=0, column=0, padx=0, pady=(0, 7.5), sticky="nsew")
+
+        right_rail_bottom = ct.CTkFrame(master=right_rail)
+        right_rail_bottom.grid(row=1, column=0, padx=0, pady=(7.5, 0), sticky="nsew")
 
     def label_button_frame_event(self, item):
         print(f"label button frame clicked: {item}")
