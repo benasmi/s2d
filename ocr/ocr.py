22490eda3242b08d889b42b4acd379ce20ddc867
Benas M <benasmiliunas@gmail.com>
2024-01-21 21:21:27 +0200

    Improve OCR by performing on different image variations

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -1,17 +1,44 @@
-from PIL import Image
-import pytesseract
 import cv2
+import easyocr
 import numpy as np
-import matplotlib.pyplot as plt
+import pytesseract
+from PIL import Image
 
-def img_path_to_string(path):
-    return image_to_string(Image.open(path))
+easyocr_reader = easyocr.Reader(['en'])
 
-def image_to_string(image):
-    gray = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
-    text = pytesseract.image_to_string(gray).strip()
+def ocr(image):
+    if isinstance(image, str):
+        image = Image.open(image)
 
-    return text
+    np_img = np.array(image)
+    scale_factor = 1.5
+    upscaled_image = cv2.resize(np_img, None, fx=scale_factor, fy=scale_factor,
+                                interpolation=cv2.INTER_LINEAR)
+
+    images = [
+        lambda: image,
+        lambda: upscaled_image,
+        lambda: cv2.cvtColor(upscaled_image, cv2.COLOR_BGR2GRAY),
+        lambda: cv2.cvtColor(np_img, cv2.COLOR_BGR2GRAY)
+    ]
+
+    return perform_ocr(images, tesseract_ocr)
 
 
-#img_path_to_string("./data/img_1.png")
+def perform_ocr(images, ocr_fn):
+    for img in images:
+        text = ocr_fn(img())
+        if text.strip():
+            return text
+    return ''
+
+
+def easy_ocr(img):
+    result = easyocr_reader.readtext(img)
+    collected_text = [element[1] for element in result]
+    return ' '.join(collected_text)
+
+
+def tesseract_ocr(img):
+    return pytesseract.image_to_string(img).strip()
+
